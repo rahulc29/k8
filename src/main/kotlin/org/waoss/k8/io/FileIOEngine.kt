4f2b514cec692d5658760115acfa8097c0f41691
rahul <rahul29112002@gmail.com>
2021-09-26 13:50:26 +0530

    Implement file IO engine to be lazily-evaluated

--- src/main/kotlin/org/waoss/k8/io/FileIOEngine.kt
+++ src/main/kotlin/org/waoss/k8/io/FileIOEngine.kt
@@ -4,7 +4,5 @@ import java.nio.file.Files
 import java.nio.file.Paths
 
 class FileIOEngine(private val name: String) : IOEngine {
-    override fun readAll(): ByteArray {
-        return Paths.get(name).let { Files.readAllBytes(it) }
-    }
+    override fun readAll(): ByteArray = lazy { Paths.get(name).let { Files.readAllBytes(it) } }.value
 }
