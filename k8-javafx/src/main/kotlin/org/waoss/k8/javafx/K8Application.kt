a4f82a8da5305da8db35c35eaf3670a0ca169134
rahul <rahul29112002@gmail.com>
2021-09-26 16:46:53 +0530

    Configure gradle for javafx subproject

--- k8-javafx/src/main/kotlin/org/waoss/k8/javafx/K8Application.kt
+++ k8-javafx/src/main/kotlin/org/waoss/k8/javafx/K8Application.kt
@@ -1,12 +1,32 @@
 package org.waoss.k8.javafx
 
 import javafx.application.Application
+import javafx.event.EventHandler
+import javafx.event.EventType
+import javafx.scene.Scene
+import javafx.scene.input.KeyEvent
+import javafx.scene.layout.StackPane
 import javafx.stage.Stage
+import org.waoss.k8.input.Key
+import org.waoss.k8.input.Keyboard
 
 class K8Application : Application() {
     override fun start(primaryStage: Stage?) {
         primaryStage?.let {
+            val parent = StackPane()
+            val scene = Scene(parent)
+            val keyboard = object : Keyboard {
+                override fun isPressed(key: Key): Boolean {
+                    TODO("Not yet implemented")
+                }
 
+                override fun nextKey(): Key {
+                    TODO("Not yet implemented")
+                }
+            }
+            scene.setOnKeyPressed {
+
+            }
         }
     }
 }
