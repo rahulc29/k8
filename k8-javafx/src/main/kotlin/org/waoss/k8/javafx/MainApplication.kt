fc72a18cb1ef79cb234b252e08809449ed3bc6cb
rahul <rahul29112002@gmail.com>
2021-09-30 22:35:48 +0530

    Implement FX-based implementation of Keyboard

--- k8-javafx/src/main/kotlin/org/waoss/k8/javafx/MainApplication.kt
+++ k8-javafx/src/main/kotlin/org/waoss/k8/javafx/MainApplication.kt
@@ -1,21 +1,32 @@
 package org.waoss.k8.javafx
 
 import javafx.application.Application
+import javafx.application.Platform
 import javafx.scene.Scene
 import javafx.scene.layout.StackPane
 import javafx.stage.Stage
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import org.waoss.k8.cpu.defaultProcessorContext
 import org.waoss.k8.gpu.positionOf
+import org.waoss.k8.input.Key
+import org.waoss.k8.input.Keyboard
 
 class MainApplication : Application() {
     override fun start(primaryStage: Stage?) {
+        val canvas = FXGraphicsContext(64, 32)
+        val container = StackPane(canvas)
+        canvas.draw(positionOf(69, 69), 1)
+        canvas.render()
         primaryStage?.let {
             it.title = "K8 Emulator"
-            val canvas = FXGraphicsContext(64, 32)
-            val container = StackPane(canvas)
-            canvas.draw(positionOf(69, 69), 1)
-            canvas.render()
             it.scene = Scene(container)
             it.show()
+        }
+        runBlocking {
+            launch {
+
+            }
         }
     }
 }
---
+++ src/test/kotlin/org/waoss/k8/cpu/FXKeyboard.kt
@@ -0,0 +1,34 @@
+package org.waoss.k8.cpu
+
+import javafx.scene.Scene
+import org.waoss.k8.input.Key
+import org.waoss.k8.input.Keyboard
+
+class FXKeyboard(private val scene: Scene) : Keyboard {
+    private var keyLastPressedName: String = ""
+
+    init {
+        scene.setOnKeyPressed {
+            val codeName = it.code.name
+            if (codeName != keyLastPressedName) {
+                keyLastPressedName = it.code.name.digitToNum()
+            }
+        }
+    }
+
+    override fun isPressed(key: Key): Boolean {
+        return keyLastPressedName == key.name
+    }
+
+    private fun String.digitToNum(): String {
+        return if (this.startsWith("DIGIT")) {
+            "NUM${this.removePrefix("DIGIT")}"
+        } else {
+            this
+        }
+    }
+
+    override fun nextKey(): Key {
+        return Key.valueOf(keyLastPressedName.digitToNum())
+    }
+}
